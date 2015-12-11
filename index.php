e80b3ee0af16f8549360b57739f593d9f2f67bdf
91.eahz@gmail.com <EHZ@Ericks-MacBook-Pro.local>
2015-12-10 19:19:41 -0500

    EH: remove temp embed styles to index

--- index.php
+++ index.php
@@ -1,14 +1,5 @@
 <?php include 'commons/head.php'; ?>
-<style type="text/css" media="screen">
-  .box{
-    width: 90%;
-    height: 90vh;
-    margin: 0 auto;
-    margin-top: 5vh;
-    overflow: auto;
-  }
-</style>
-<div class="box">
+
 
 <?php include 'sidebar.php' ?>
 </div>
