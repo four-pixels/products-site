68bd1f739da0403c4dd12cd32ba05308e224ddf1
91.eahz@gmail.com <EHZ@Ericks-MacBook-Pro.local>
2015-12-10 21:37:33 -0500

    EH:styling added for main product page

--- productPage.php
+++ productPage.php
@@ -1,3 +1,15 @@
 <?php include 'commons/head.php'; ?>
-<section class="product"></section>
+<section class="product">
+  <div class="product__info">
+    <div class="product__thumbnails__container"></div>
+    <div class="product__description">
+      <div class="product__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima unde ex consequatur minus dolor eaque, totam nihil autem fugiat est dolorem perspiciatis numquam eius, libero harum. Iusto voluptatem architecto possimus.</div>
+      <div class="product__price">Starting at <span>$43,700</span></div>
+    </div>
+  </div>
+  <div class="product__img-container">
+    <h1 id="product__title">THUNDERBIRD</h1>
+    <img src="images/thunderbird/BC_MY14_Thunderbird_PR_rhs.png" alt="">
+  </div>
+</section>
 <?php include 'commons/footer.php'; ?>
