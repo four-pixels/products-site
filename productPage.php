01ecb31ef8c7cf8d9c8fa63ec94d10b2ceb58532
91.eahz@gmail.com <EHZ@Ericks-MacBook-Pro.local>
2015-12-11 03:53:08 -0500

    EH: thumbnails added, is-hidden class added to base

--- productPage.php
+++ productPage.php
@@ -1,7 +1,13 @@
 <?php include 'commons/head.php'; ?>
 <section class="product">
   <div class="product__info">
-    <div class="product__thumbnails__container"></div>
+    <div class="product__thumbnails__container">
+      <ul>
+        <li class="thumbnail__image thumbnail__image-1"></li>
+        <li class="thumbnail__image thumbnail__image-2"></li>
+        <li class="thumbnail__image thumbnail__image-3"></li>
+      </ul>
+    </div>
     <div class="product__description">
       <div class="product__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima unde ex consequatur minus dolor eaque, totam nihil autem fugiat est dolorem perspiciatis numquam eius, libero harum. Iusto voluptatem architecto possimus.</div>
       <div class="product__price">Starting at <span>$43,700</span></div>
@@ -9,7 +15,7 @@
   </div>
   <div class="product__img-container">
     <h1 id="product__title">THUNDERBIRD</h1>
-    <img src="images/thunderbird/BC_MY14_Thunderbird_PR_rhs.png" alt="">
+    <img id="product__image" src="images/thunderbird/BC_MY14_Thunderbird_PR_rhs.png" alt="">
   </div>
 </section>
 <?php include 'commons/footer.php'; ?>
