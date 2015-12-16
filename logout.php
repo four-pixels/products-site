5b51420498b35aecd40b8c1fb21b2c75a5197b38
Rene Ramirez <rene.ramirez@fersz.com>
2015-12-16 00:36:30 -0500

    add logout style

--- logout.php
+++ logout.php
@@ -1,14 +1,14 @@
 <?php
 
-//SESSION HAS TO BE DESTROYYED BEFORE RENDERING THE HEADER
-// require_once 'session/SessionManager.php';
 require_once 'configFolder/sessions.php';
-
 destroySession();
-
-
 include 'commons/head.php';
 ?>
-
+<section id="logout">
+  <div class="logout-box">
+    <h1>Thanks for Visiting</h1>
+    <h2>hope to see you soon</h2>
+  </div>
+</section>
 
 <?php include 'commons/footer.php'; ?>
