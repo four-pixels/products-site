b7f15924a391f0dadc0b5839d18632e3836e7f70
Rene Ramirez <rene.ramirez@fersz.com>
2015-12-16 01:03:17 -0500

    Add References to the images content and design

--- logout.php
+++ logout.php
@@ -8,6 +8,11 @@ include 'commons/head.php';
   <div class="logout-box">
     <h1>Thanks for Visiting</h1>
     <h2>hope to see you soon</h2>
+    <div class="copyright">
+      <p>Content and Images retrieved from <a class="link" href="http://www.triumph-motorcycles.ca/">Triumph</a></p>
+      <p>Design inspiration retrieved from <a class="link" href="https://dribbble.com/shots/2198944-Motorcycles/">Dribbble</a></p>
+    </div>
+    <h3>Live demo at <br> <a class="link" href="http://bike.4pixels.co/">bike.4pixels.co</a></h3>
   </div>
 </section>
 
