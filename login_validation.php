70e91a126967f4c31efc18ba221c9be2ce430c99
Rene Ramirez <rene.ramirez@fersz.com>
2015-12-17 13:19:17 -0500

    remove not used code

--- login_validation.php
+++ login_validation.php
@@ -13,21 +13,3 @@ if($userCheck === false){
 }
 exit;
 
-require_once 'database/Database.php';
-require_once 'session/SessionManager.php';
-$db = new FourPixels\Database\Database();
-$userForm = $_POST['user'];
-$userResult = $db->getUserByPasswordAndUsernameOrEmail($userForm['password'], $userForm['username']);
-$session = new FourPixels\Session\SessionManager();
-if ($userResult['hasError'] !== '') {
-  $error = $userResult['hasError'];
-  $session->setFlashMessage($error);
-  header('Location: http://' . $_SERVER['HTTP_HOST'] . '/login.php', true, 301);
-  exit; // THIS WILL STOP PHP to execute any code below this like
-} else {
-  $session->destroy(); //ERASE if something wass in the SESSION to allow a new one
-  $session = new FourPixels\Session\SessionManager();
-  $session->setSession($userResult['result']);
-  header('Location: http://' . $_SERVER['HTTP_HOST'] . '/index.php', true, 301);
-  exit; // THIS WILL STOP PHP to execute any code below this like
-}
